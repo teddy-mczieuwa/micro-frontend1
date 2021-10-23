a2e0d505e2a404ad1c058e088c0691f30a55839b
Teddy_McZieuwa <edwardalozieuwa@gmail.com>
2021-10-23 09:47:48 +0100

    added publicPath to webpack.prod.js

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -8,7 +8,8 @@ const domain = process.env.PRODUCTION_DOMAIN
 const prodConfig = {
     mode: 'productions',
     output: {
-        filename: '[name].[contenthash].js'
+        filename: '[name].[contenthash].js',
+        publicPath: '/container/latest'
     },
     plugins:[
         new ModuleFederationPlugin({
